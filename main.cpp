20465ca27df12d7a1265145e4889d62a880448b1
buihuy1203 <buihuy1203@gmail.com>
2024-11-06 20:52:49 +0700

    Parallel Update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <omp.h>
+#include <chrono>
 #include "process/RGBtoYCrCB.cpp"
 #include "process/Blur.cpp"
 #include "process/BrightNess.cpp"
 #include "process/Saturation.cpp"
 #include "process/Sharpness.cpp"
+#include "parallelprocess/RGBtoYCrCB.cpp"
+#include "parallelprocess/Blur.cpp"
+#include "parallelprocess/BrightNess.cpp"
+#include "parallelprocess/Saturation.cpp"
+#include "parallelprocess/Sharpness.cpp"
 using namespace std;
 using namespace cv;
 int main() {
@@ -18,13 +25,27 @@ int main() {
         cout << "Khong the doc anh." << endl;
         return -1;
     }
-
+    auto start1 = chrono::high_resolution_clock::now(); 
     // Hiển thị ảnh
     imshow("Original", image1);
+    imshow("RGBtoYCrCb image", YCrCBImage(image1));
     imshow("Saturation image", Saturation(image1, 1));
     imshow("Sharpness image", Sharpness(image1, 10));
     imshow("Blur image", BlurImage(image1, 1));
     imshow("Brightness image", BrightNess(image1, -100));
+    auto end1 = chrono::high_resolution_clock::now(); 
+    chrono::duration<double> duration1 = end1 - start1;
+    cout <<"Thoi gian thuc thi tuan tu: "<<duration1.count()<<"s"<<endl;
+    auto start2 = chrono::high_resolution_clock::now(); 
+    // Hiển thị ảnh
+    imshow("Original", image1);
+    imshow("Saturation image parallel", ParallelSaturation(image1, 1));
+    imshow("Sharpness image parallel", ParallelSharpness(image1, 10));
+    imshow("Blur image parallel", ParallelBlurImage(image1, 1));
+    imshow("Brightness image parallel", ParallelBrightNess(image1, -100));
+    auto end2 = chrono::high_resolution_clock::now(); 
+    chrono::duration<double> duration2 = end2 - start2;
+    cout <<"Thoi gian thuc thi song song: "<<duration2.count()<<"s"<<endl;
     waitKey(0);
 
     return 0;
---
+++ parallelprocess/RGBtoYCrCB.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <omp.h>
+using namespace std;
+using namespace cv;
+
+Mat ParallelYCrCBImage(const Mat &input) {
+    int height = input.rows;
+    int width = input.cols;
+
+    Mat result = Mat::zeros(height, width, CV_8UC3);
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            Vec3b pixel = input.at<Vec3b>(y, x);
+            uchar blue = pixel[0];
+            uchar green = pixel[1];
+            uchar red = pixel[2];
+
+            uchar Y = static_cast<uchar>(0.299 * red + 0.587 * green + 0.114 * blue);
+            uchar Cb = static_cast<uchar>(128 + (blue - Y) * 0.564);
+            uchar Cr = static_cast<uchar>(128 + (red - Y) * 0.713);
+
+            result.at<Vec3b>(y, x) = Vec3b(Y, Cb, Cr);
+        }
+    }
+    return result;  
+}
